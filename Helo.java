87a3bf2302beeb0a5ec11b964f0837fe1330639a
f-e-e-b <61569927+f-e-e-b@users.noreply.github.com>
2025-02-25 22:57:51 +0000

    a

--- Helo.java
+++ Helo.java
@@ -1,5 +1,6 @@
-public Class Helo{
+ public class Helo{
     public static void main(String[] args){
-        
+        System.out.println("ME WHEN SZA: :O");
+        System.out.println("I Hate to be the one... hate to be the one doing the MOOOSSSST it's what it takes, yeah");
     }
 }
