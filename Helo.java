1339242ead9b8336d6ea0e29a1558f34038ffbf9
f-e-e-b <61569927+f-e-e-b@users.noreply.github.com>
2025-03-04 22:17:15 +0000

    MARIAS

--- Helo.java
+++ Helo.java
@@ -1,6 +1,7 @@
  public class Helo{
     public static void main(String[] args){
-        System.out.println("ME WHEN SZA: :O");
-        System.out.println("I Hate to be the one... hate to be the one doing the MOOOSSSST it's what it takes, yeah");
+        System.out.println("ME WHEN THE MARIAS :0");
+        System.out.println("If in the morning, I am with you\n and in the evening i am there\n and if it's you that's always waiting\n kissing the flowers in my hair");
+        System.out.println("Why do you think I have another\n when you have always been the one\n your paranoia is annoying\n now all I wanna do is run away");
     }
 }
